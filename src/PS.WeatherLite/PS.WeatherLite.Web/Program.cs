03f06494933f68dc295cbc1d0965c49dc344492c
Alexander <sany199574@gmail.com>
2025-04-19 08:03:08 +0300

    Удалены лишнии строки кода

--- src/PS.WeatherLite/PS.WeatherLite.Web/Program.cs
+++ src/PS.WeatherLite/PS.WeatherLite.Web/Program.cs
@@ -7,8 +7,6 @@ using PS.WeatherLite.Web.Storage;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
-
 // Настройка конфигурации
 builder.Services.Configure<ShortenerSettings>(builder.Configuration.GetSection("Shortener"));
 
